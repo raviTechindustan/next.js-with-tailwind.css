cc04d206bc36930f017d2d71d438b0e86a2249b8
raviTechindustan <ravi.techindustan@gmail.com>
2020-05-27 19:51:56 +0530

    icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import "../assets/style.css"
 import '@fortawesome/fontawesome-svg-core/styles.css';
-
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
+
 
 import Home from '../pages/Home'
 
