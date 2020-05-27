bb98a4610325c46a059d1e7e1e82cbbd06ee592c
raviTechindustan <ravi.techindustan@gmail.com>
2020-05-27 19:03:23 +0530

    add globally css

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import "../assets/style.css"
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
-import "../assets/style.css"
+
 import Home from '../pages/Home'
 import Navbar from '../components/Navbar'
 export default function App() {
