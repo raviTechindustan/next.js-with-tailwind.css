fc60499d1b6bd254c28f2f51e886ee3a8707f02d
raviTechindustan <ravi.techindustan@gmail.com>
2020-05-27 19:30:20 +0530

    font awesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../assets/style.css"
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    lineHeight: {
+      '15':'3.75rem'
+    },
+    colors: {
+      'brand-blue': '#fc8181'
+    },
+    
+    extend: {
+      fontSize: {
+        xl: '12rem',
+      },  
+    },
+  },
+  variants: {},
+  plugins: [],
+}
